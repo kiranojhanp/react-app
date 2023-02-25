84b6c52dc120f2b0436751a59d217bb6822272ba
Kiran Ojha <kiranojha1226@gmail.com>
2023-02-25 14:42:05 +0545

    feat: add base

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ function renderChunks(deps: Record<string, string>) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/react-app/",
+  base: "./",
   build: {
     sourcemap: false,
     rollupOptions: {
